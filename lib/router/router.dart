74fe670350aed7db1591e0050ae4cb2fd4f300b2
K.Shrestha <shrestha.kishor@grrow.jp>
2024-08-28 20:45:35 +0900

    Add image assets and buttons

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
 import 'package:transqr/view/home_view.dart';
 
-final router = GoRouter(
-    routes: [GoRoute(path: "/", builder: (context, state) => const Main())]);
+final router =
+    GoRouter(routes: [GoRoute(path: "/", builder: (context, state) => Main())]);
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Main extends StatelessWidget {
-  const Main({super.key});
+  Main({super.key});
+
+  final Widget send = SvgPicture.asset(
+    'lib/assets/send.svg',
+  );
+
+  final Widget scan = SvgPicture.asset(
+    'lib/assets/scan.svg',
+    height: 512,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +23,61 @@ class Main extends StatelessWidget {
           height: double.infinity,
           width: double.infinity,
           padding: const EdgeInsets.all(10),
-          child: const Column(
+          child: Column(
             children: [
+              Expanded(child: TextSvgButton(text: "Scan", child: scan)),
               Expanded(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text("test")])),
-              Expanded(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text("test")]))
+                child: TextSvgButton(
+                  text: "Send",
+                  child: send,
+                ),
+              )
             ],
           )),
     );
+  }
+}
+
+class TextSvgButton extends StatelessWidget {
+  TextSvgButton({super.key, required this.child, required this.text});
+
+  final Widget child;
+  String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Button(
+        onPressed: () {},
+        child: child,
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      Text(text)
+    ]);
+  }
+}
+
+class Button extends StatelessWidget {
+  VoidCallback onPressed;
+  Widget child;
+
+  Button({super.key, required this.onPressed, required this.child});
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Container(
+        height: 200,
+        width: 200,
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                //borderRadius: BorderRadius.zero, //Rectangular border
+              ),
+            ),
+            onPressed: onPressed,
+            child: child));
   }
 }
