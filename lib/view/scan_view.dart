070d451ad1806294315885113c57e0ce8ffa70bf
K.Shrestha <shrestha.kishor@grrow.jp>
2024-09-02 20:30:26 +0900

    Add added qr code scanner

--- lib/view/scan_view.dart
+++ lib/view/scan_view.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class ScanView extends StatefulWidget {
   const ScanView({super.key});
@@ -8,9 +11,85 @@ class ScanView extends StatefulWidget {
   State<ScanView> createState() => _ScanViewState();
 }
 
-class _ScanViewState extends State<ScanView> {
+class _ScanViewState extends State<ScanView> with WidgetsBindingObserver {
+  final MobileScannerController controller = MobileScannerController(
+    // required options for the scanner
+    autoStart: false,
+    torchEnabled: false,
+    useNewCameraSelector: true,
+  );
+
+  Barcode? _barcode;
+
+  StreamSubscription<Object?>? _subscription;
+
+  void handleBarcodeRead(BarcodeCapture data) {
+    if (mounted) {
+      setState(() {
+        _barcode = data.barcodes.firstOrNull;
+      });
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // If the controller is not ready, do not try to start or stop it.
+    // Permission dialogs can trigger lifecycle changes before the controller is ready.
+    if (!controller.value.isInitialized) {
+      return;
+    }
+
+    switch (state) {
+      case AppLifecycleState.detached:
+      case AppLifecycleState.hidden:
+      case AppLifecycleState.paused:
+        return;
+      case AppLifecycleState.resumed:
+        // Restart the scanner when the app is resumed.
+        // Don't forget to resume listening to the barcode events.
+        _subscription = controller.barcodes.listen(handleBarcodeRead);
+
+        unawaited(controller.start());
+      case AppLifecycleState.inactive:
+        // Stop the scanner when the app is paused.
+        // Also stop the barcode events subscription.
+        unawaited(_subscription?.cancel());
+        _subscription = null;
+        unawaited(controller.stop());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Start listening to lifecycle changes.
+    WidgetsBinding.instance.addObserver(this as WidgetsBindingObserver);
+
+    // Start listening to the barcode events.
+    _subscription = controller.barcodes.listen(handleBarcodeRead);
+
+    // Finally, start the scanner itself.
+    unawaited(controller.start());
+  }
+
+  @override
+  Future<void> dispose() async {
+    // Stop listening to lifecycle changes.
+    WidgetsBinding.instance.removeObserver(this);
+    // Stop listening to the barcode events.
+    unawaited(_subscription?.cancel());
+    _subscription = null;
+    // Dispose the widget itself.
+    super.dispose();
+    // Finally, dispose of the controller.
+    await controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.sizeOf(context).height;
+    final double screenWidth = MediaQuery.sizeOf(context).height;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Scan'),
@@ -22,6 +101,53 @@ class _ScanViewState extends State<ScanView> {
           ),
         ),
         body: SafeArea(
-            child: Container(height: double.infinity, child: Text("Scan"))));
+            child: Container(
+                padding: const EdgeInsets.all(20),
+                height: screenHeight,
+                width: screenWidth,
+                child: Stack(
+                  children: [
+                    MobileScanner(
+                      controller: controller,
+                      errorBuilder: (context, error, child) {
+                        return const Text("Error");
+                      },
+                      fit: BoxFit.contain,
+                    ),
+                    Text(_barcode?.rawValue ?? '')
+                    // Extra(controller: controller),
+                  ],
+                ))));
+  }
+}
+
+class Extra extends StatelessWidget {
+  const Extra({
+    super.key,
+    required this.controller,
+  });
+
+  final MobileScannerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        alignment: Alignment.bottomCenter,
+        height: 100,
+        color: Colors.black.withOpacity(0.4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // ToggleFlashlightButton(controller: controller),
+            // StartStopMobileScannerButton(controller: controller),
+            // Expanded(child: Center(child: _buildBarcode(_barcode))),
+            // SwitchCameraButton(controller: controller),
+            // AnalyzeImageFromGalleryButton(controller: controller),
+          ],
+        ),
+      ),
+    );
   }
 }
